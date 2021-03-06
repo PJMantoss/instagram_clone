53bfa89d82c009844c68f972190cbf163660e0b6
pjmantoss <joelptoss@gmail.com>
2021-03-06 15:05:18 +0100

    firebase app integration

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,1 +1,10 @@
-const config = 
+const firebaseConfig = {
+      apiKey: "AIzaSyC1MIR55EmC-37J7oDhQ3xNWct24HYzlNo",
+      authDomain: "instagram-clone-9b6ce.firebaseapp.com",
+      projectId: "instagram-clone-9b6ce",
+      storageBucket: "instagram-clone-9b6ce.appspot.com",
+      messagingSenderId: "590328505704",
+      appId: "1:590328505704:web:a2d6af91107b56901422dd"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
