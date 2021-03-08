213f541ad83b423f58b013ace46914a9c96f636c
pjmantoss <joelptoss@gmail.com>
2021-03-08 18:17:31 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, {lazy, Suspense} from 'react';
 import './App.css';
 
 const Dashboard = lazy(() => import('./pages/dashboard'));
-const Login = lazy(() => import('/pages/login'));
-const SignUp = lazy(() => import('/pages/signup'));
+const Login = lazy(() => import('./pages/login'));
+const SignUp = lazy(() => import('./pages/signup'));
 const Profile = lazy(() => import('/pages/profile'));
 const NotFound = lazy(() => import('/pages/not-found'));
 
