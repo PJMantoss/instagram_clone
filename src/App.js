a1cdaf57554fb28a6e36db873d28d9b761e2e335
pjmantoss <joelptoss@gmail.com>
2021-03-08 18:13:49 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {lazy, Suspense} from 'react';
 import './App.css';
 
+const Dashboard = lazy(() => import('/pages/'))
+
 function App() {
   return (
       <h1 className="text-red-500">Learn React</h1>
