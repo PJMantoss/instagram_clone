51a2840d972c523d76f6b4e07141756f4e9bcff9
pjmantoss <joelptoss@gmail.com>
2021-03-08 18:17:01 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const Dashboard = lazy(() => import('/pages/dashboard'));
 const Login = lazy(() => import('/pages/login'));
 const SignUp = lazy(() => import('/pages/signup'));
 const Profile = lazy(() => import('/pages/profile'));
-const NotFound = lazy(() => import('/pages/notfound'));
+const NotFound = lazy(() => import('/pages/not-found'));
 
 function App() {
   return (
