f9ed008f5ec08de098676791b9a9d1fc2e564820
pjmantoss <joelptoss@gmail.com>
2021-03-10 13:06:48 +0100

    update login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Login(){
     return(
         <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-            Hello from Login!
+            <div className="w-3/5"></div>
+
+            <div className="w-2/5"></div>
         </div>
     )
 }
