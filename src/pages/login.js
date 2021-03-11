7044af03098b8f3bae292193f5db64fc23d1225b
pjmantoss <joelptoss@gmail.com>
2021-03-11 23:28:23 +0100

    update login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -6,6 +6,7 @@ export default function Login(){
     useEffect(() => {
         document.title = 'Login - Instagram';
     }, [])
+    
     return(
         <div className="container flex mx-auto max-w-screen-md items-center h-screen">
             <div className="flex w-3/5">
