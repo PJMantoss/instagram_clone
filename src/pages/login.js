4507e49226f6a9a3dfd1403e59630c857fd08e4a
pjmantoss <joelptoss@gmail.com>
2021-03-11 23:27:20 +0100

    update login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
 
 export default function Login(){
-    useEffect(() => {}, [])
+    useEffect(() => {
+        document.title = '';
+    }, [])
     return(
         <div className="container flex mx-auto max-w-screen-md items-center h-screen">
             <div className="flex w-3/5">
