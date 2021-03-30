c658efffb6947772bc6ac76d18f3d74d95ffac96
pjmantoss <joelptoss@gmail.com>
2021-03-30 14:32:10 +0100

    update signup.js

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -10,9 +10,9 @@ export default function SignUp(){
                     <h1 className="flex justify-center w-full">
                         <img src="./images/logo.png" alt="instagram" className="mt-2 w-6/12 mb-4" />
                     </h1>
-                    {error && <p className="mb-4 text-xs text-red-500">{error}</p>}
+                    {/* {error && <p className="mb-4 text-xs text-red-500">{error}</p>} */}
 
-                    <form onSubmit={handleChange} method="POST">
+                    <form method="POST">
                         <input 
                             aria-label="Enter your username" 
                             className="text-sm text-gray w-full mr-3 py-5 px-4 h-2 border bg-gray-background rounded mb-2" 
