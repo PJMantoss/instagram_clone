af53e695fe3580e1e6c19d1774fbbee79524290e
pjmantoss <joelptoss@gmail.com>
2021-03-30 14:36:03 +0100

    update signup.js

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constants/routes';
 
