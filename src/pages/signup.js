b10c97cf38da8f3927b73df2c514aab91f197881
pjmantoss <joelptoss@gmail.com>
2021-03-30 14:09:59 +0100

    update signup.js

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -4,6 +4,10 @@ import * as ROUTES from '../constants/routes';
 
 export default function SignUp(){
     return(
-        <div className="container flex mx-auto max-w-screen-md items-center h-screen"></div>
+        <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+            <div className="flex flex-col w-2/5">
+                
+            </div>
+        </div>
     )
 }
