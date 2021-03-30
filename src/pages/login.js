d3188346f97f59cfb5c9242742c1b9eac09ee4c7
pjmantoss <joelptoss@gmail.com>
2021-03-30 14:03:59 +0100

    update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -18,6 +18,7 @@ export default function Login(){
 
         try {
             await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+            history.push(ROUTES.DASHBOARD);
         }catch(err){
             setEmailAddress('');
             setPassword('');
