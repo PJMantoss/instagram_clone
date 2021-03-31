bd796d4cc943b228446b7ec58a677948a96acdb4
pjmantoss <joelptoss@gmail.com>
2021-03-31 10:58:34 +0100

    update logic in signup.js

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -51,6 +51,7 @@ export default function SignUp(){
             setFullName('');
             setEmailAddress('');
             setPassword('');
+            setError(err.message);
         }
     }
 
