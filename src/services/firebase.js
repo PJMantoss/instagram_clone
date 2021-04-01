bf131061f614d2175b600ac417dbe74e986ac13d
pjmantoss <joelptoss@gmail.com>
2021-04-01 11:27:18 +0100

    update services

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,3 +1,5 @@
 import { firebase, FieldValue } from '../lib/firebase';
 
-export async function doesUserExist(username){}
+export async function doesUserExist(username){
+    const result = await firbase.
+}
