932bf178b003eff9854e76bcafba5acce5cf90e6
pjmantoss <joelptoss@gmail.com>
2021-04-03 20:04:49 +0100

    update dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
-import Header from '../components/header';
-import Timeline from '../components/timeline';
-import Sidebar from '../components/sidebar';
+import Header from '../components/header.js';
+import Timeline from '../components/timeline.js';
+import Sidebar from '../components/sidebar/index.js';
 
 export default function Dashboard(){
     useEffect(() => {
         document.title = 'Instagram';
     }, []);
-    
+
     return(
         <h1>Hello from Dashboard!</h1>
     )
