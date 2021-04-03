3670bc10b9ab9356077f02bab3f46a52a98a5ffa
pjmantoss <joelptoss@gmail.com>
2021-04-03 19:50:37 +0100

    update dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import Header from '../components/header';
+import Timeline from '../components/timeline';
+
 export default function Dashboard(){
     return(
         <h1>Hello from Dashboard!</h1>
