ea6ae3918461d5b194b75254ca69838d46d6328a
pjmantoss <joelptoss@gmail.com>
2021-04-03 20:16:04 +0100

    update dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -9,7 +9,7 @@ export default function Dashboard(){
     }, []);
 
     return(
-        <div className="bg-gray-200">
+        <div className="bg-gray-100">
             <Header />
             <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
                 <Timeline />
