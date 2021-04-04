2a27ce1c598655aed8c6ac855c6d3a7535241001
pjmantoss <joelptoss@gmail.com>
2021-04-04 09:01:10 +0100

    update App.js

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -4,7 +4,7 @@ export default function NotFound(){
     return(
         <div className="bg-gray">
             <div className="mx-auto max-w-screen-lg">
-                <p className="text-enter text-2xl"></p>
+                <p className="text-enter text-2xl">Not Found</p>
             </div>
         </div>
     )
