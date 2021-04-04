8532916aa1c95ab2526f67e20de7bff611c3731b
pjmantoss <joelptoss@gmail.com>
2021-04-04 08:58:36 +0100

    update App.js

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function NotFound(){
     return(
         <div className="bg-gray">
-            <div></div>
+            <div className="mx-auto max-w-screen-lg">
+                <p></p>
+            </div>
         </div>
     )
 }
