ab800dfbb8caa5ce96aacd91c4acb732919d9e70
pjmantoss <joelptoss@gmail.com>
2021-04-04 09:10:10 +0100

    update App.js

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
 
 export default function NotFound(){
-    useEffect(() => {},[])
-    
+    useEffect(() => {
+        document.title = "404 - Not Found!";
+    },[]);
+
     return(
         <div className="bg-gray-100">
             <div className="mx-auto max-w-screen-lg">
