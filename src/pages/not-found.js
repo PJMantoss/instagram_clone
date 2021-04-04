fb463e2239a0b129259d170c14918d2315f324cb
pjmantoss <joelptoss@gmail.com>
2021-04-04 09:08:11 +0100

    update App.js

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 
 export default function NotFound(){
+    useEffect(() => {},[])
+    
     return(
         <div className="bg-gray-100">
             <div className="mx-auto max-w-screen-lg">
-                <p className="text-enter text-2xl">Not Found</p>
+                <p className="text-enter text-2xl">Not Found!</p>
             </div>
         </div>
     )
