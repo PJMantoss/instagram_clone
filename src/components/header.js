d1eb2c07f59595a9750cb6e844a92849cc80acd5
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-06 17:55:50 +0100

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -39,7 +39,7 @@ export default function Header(){
                                 >
                                     Sign Out
                                 </button>
-                                <div>
+                                <div className="flex items-center cursor-center">
                                         <Link></Link>
                                 </div>
                              </>
