f0b1807905709870cdb2e049dea361dfcfe7903b
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-06 19:02:30 +0100

    update use-auth-listener.js

--- hooks/use-auth-listener.js
+++ hooks/use-auth-listener.js
@@ -2,5 +2,5 @@ import { useState, useEffect, useContext } from 'react';
 import FirebaseContext from '../context/firebase';
 
 export default function useAuthListener(){
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem()));
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
 }
