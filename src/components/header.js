233f6e16fca9d6ca334b2cb59b2ac9127f6e6ed4
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-06 16:26:16 +0100

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ export default function Header(){
         <header className="h-16 bg-white border-b mb-8">
             <div className="container mx-auto max-width-lg h-full">
                 <div className="flex justify-between h-full">
-                    <div className="text-gray-700 text-center flex items-center align-items"></div>
+                    <div className="text-gray-700 text-center flex items-center align-items cursor-pointer"></div>
                 </div>
             </div>
         </header>
