8d383830caf8772c55629b6ac89f28c4b5394761
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-06 16:22:41 +0100

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,8 @@ export default function Header(){
     return(
         <header className="h-16 bg-white border-b mb-8">
             <div className="container mx-auto max-width-lg h-full">
+                <div className="flex justify-between h-full">
+                </div>
             </div>
         </header>
     )
