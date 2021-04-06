0d138923e999b468eec79b95570dcd75f1bc73be
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-06 18:01:54 +0100

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -43,8 +43,8 @@ export default function Header(){
                                         <Link to={`/p/${user.displayName}`}>
                                             <img
                                                 className="rounded-full h-8 w-8 flex"
-                                                src={}
-                                                alt={}
+                                                src={`/images/avatars/${user.displayName}.jpg`}
+                                                alt={`${user.displayName} profile picture`}
                                             />
                                         </Link>
                                 </div>
