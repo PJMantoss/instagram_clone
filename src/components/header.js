e94197f55be3612b2f9b9dfbba58343a9ff6c28a
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-06 16:19:47 +0100

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 export default function Header(){
     return(
         <header className="h-16 bg-white border-b mb-8">
-        
+            <div className="">
+            </div>
         </header>
     )
 }
