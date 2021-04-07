ae7da1fd7a4a339d8ac9efe6bed62a4e3566a6f5
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-07 13:29:15 +0100

    Update index.js

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { useUser } from '../hooks/use-user.js'
+import { useUser } from '../../hooks/use-user.js'
 
 export default function Sidebar(){
-    const { docId, userId, following, username, fullName } = useUser();
+    const user = useUser();
     
     return(<p>I am the Sidebar</p>)
 }
