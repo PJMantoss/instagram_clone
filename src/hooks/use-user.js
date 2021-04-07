f613975fa05b42fc2d331a580e91e37e8368c309
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-07 09:55:57 +0100

    Update use-user

--- src/hooks/use-user.js
+++ src/hooks/use-user.js
@@ -5,5 +5,7 @@ export default function useUser(){
   const [activeUser, setActiveUser] = useState({});
   const { user } = useContext(UserContext);
   
+  useEffect(() => {}, []);
+  
   return {user: activeUser};
 };
