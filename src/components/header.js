b71344371e3c4abf6b2099c748fcd96164830b30
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-07 09:39:36 +0100

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+
 import FirebaseContext from '../context/firebase';
+import UserContext from '../context/user';
 import * as ROUTES from '../constants/routes';
 
 export default function Header(){
