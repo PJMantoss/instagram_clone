2b07883ba9e7b33b497ead3c5c3b48fb9f676755
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-07 09:59:28 +0100

    Update use-user

--- src/hooks/use-user.js
+++ src/hooks/use-user.js
@@ -1,5 +1,6 @@
 import { useState, useContext, useEffect } from 'react';
 import UserContext from '../context/user';
+import { getUserByUserId } from '..services/firebase';
 
 export default function useUser(){
   const [activeUser, setActiveUser] = useState({});
