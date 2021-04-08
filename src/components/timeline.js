c11474f9b5ec91ee6460dd1303552251d9eae504
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-08 08:43:02 +0100

    Update timeline.js

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -12,7 +12,7 @@ export default function Timeline(){
             {!photos ? (
                 <>
                     {[...new, Array(4)].map((_, index) => (
-                          <Skeleton count={4} width={640} height={500} className="mb-5" />
+                          <Skeleton key={index} count={1} width={640} height={500} className="mb-5" />
                     ))}
                 </>
              ) : (
