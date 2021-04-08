ac6119aded493537255c882704f724bd1138f506
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-08 08:40:15 +0100

    Update timeline.js

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -5,11 +5,14 @@ import Skeleton from 'react-loading-skeleton';
 //   - https://www.npmjs.com/package/react-loading-skeleton
 
 export default function Timeline(){
-    const photos = [1,2,3,4,5];
+    const photos = null;
     
     return(
         <div className="container col-span-2">
             {!photos ? (
+                <>
+                    {[...new, Array(4)].map(_, index) => ()}
+                </>
                 <Skeleton count={4} width={640} height={500} className="mb-5" />
              ) : (
                  photos.map(photo => 'I will be a photo')
