2182aa5c31521d4b7b8d66c67cc688834e71b977
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-08 08:56:11 +0100

    Update use-followed-users-photos.js

--- src/hooks/use-followed-users-photos.js
+++ src/hooks/use-followed-users-photos.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import UserContext from '../context/user';
 
 export default function useFollowedUsersPhotos(){
+  const { uid } = useContext(UserContext);
   const [photos, setPhotos] = useState();
   
   return(<div></div>)
