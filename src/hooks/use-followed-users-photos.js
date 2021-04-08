a663f76024bce12ec215f08d343855fd6b71b905
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-08 09:13:13 +0100

    update use-followed-users-photos.js

--- src/hooks/use-followed-users-photos.js
+++ src/hooks/use-followed-users-photos.js
@@ -5,6 +5,9 @@ export default function useFollowedUsersPhotos(){
   const { user } = useContext(UserContext);
   const [photos, setPhotos] = useState();
   
+  const userId = user.uid;
+  userId = '';
+  
   useEffect(() => {
     async function getTimelinePhotos(){};
   }, []);
