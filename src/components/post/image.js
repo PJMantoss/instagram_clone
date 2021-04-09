cfd748d3a146ab5d43db177b8b95c47f211bd20d
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-09 10:31:37 +0100

    Update image.js

--- src/components/post/image.js
+++ src/components/post/image.js
@@ -1,3 +1,8 @@
 import React from 'react';
 
-export default function Image({ src, caption }){}
+export default function Image({ src, caption }){
+  return(
+    <div className="post__img">
+    </div>
+  )
+}
