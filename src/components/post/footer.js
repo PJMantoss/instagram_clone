f5cbb0c40326aaa791eefe05bc8e801ed675646c
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-09 10:43:09 +0100

    Update footer.js

--- src/components/post/footer.js
+++ src/components/post/footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Footer({ caption, username }){
   return(
-    <div className="">
-        <span className=""></span>
+    <div className="p-4 pt-2 pb-0">
+        <span className="mr-1 font-bold"></span>
         <span className=""></span>
     </div>
   )
