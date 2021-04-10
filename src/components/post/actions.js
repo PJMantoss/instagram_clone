f7abe6dd323b8c1b4af26032798c7e965892d0e0
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-10 15:34:26 +0100

    Update actions.js

--- src/components/post/actions.js
+++ src/components/post/actions.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import FirebaseContext from '../../context/firebase';
 import UserContext from '../../user';
 
-export default function Actions(){
+export default function Actions({ docId, totalLikes, likedPhoto, handleFocus }){
   return(
     <div></div>
   );
