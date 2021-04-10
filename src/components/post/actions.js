d0ed038df4c5d721bd0de0aca26ea8e14380d236
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-10 15:46:04 +0100

    Update actions.js

--- src/components/post/actions.js
+++ src/components/post/actions.js
@@ -1,11 +1,11 @@
 import React, { useState, useContext } from 'react';
 import FirebaseContext from '../../context/firebase';
-import UserContext from '../../user';
+import UserContext from '../../context/user';
 
 export default function Actions({ docId, totalLikes, likedPhoto, handleFocus }){
   return(
     <>
-        <div className="">
+        <div className="flex justify-between p4-">
             <div className=""></div>
         </div>
     </>
