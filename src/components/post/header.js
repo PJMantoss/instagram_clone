9c7f2b133d115fe3786e31344d3c457ba71df8b8
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-10 16:36:04 +0100

    Update header.js

--- src/components/post/header.js
+++ src/components/post/header.js
@@ -1,4 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Header(){};
+export default function Header({ username }){
+  return(
+    <div>
+    </div>
+  )
+};
