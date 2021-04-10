9eafb890613da68d8c85cf22380ab380edb74be8
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-10 16:46:16 +0100

    Update header.js

--- src/components/post/header.js
+++ src/components/post/header.js
@@ -5,6 +5,7 @@ export default function Header({ username }){
   return(
     <div className="flex border-b h-4 p-4 py-8">
         <div className="flex items-center">
+            <img src{} alt={} />
             <p className="font-bold">{username}</p>
         </div>
     </div>
