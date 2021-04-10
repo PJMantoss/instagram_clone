523bd75ae9832a4831449b6eb76936c836430866
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-10 16:54:32 +0100

    Update header.js

--- src/components/post/header.js
+++ src/components/post/header.js
@@ -6,7 +6,11 @@ export default function Header({ username }){
     <div className="flex border-b h-4 p-4 py-8">
         <div className="flex items-center">
             <Link to={`./p/${username}`} className="flex items-center">
-                <img src{`./images/avatars/${username}.jpg`} alt={`${username} profile picture`} />
+                <img 
+                    className="" 
+                    src={`./images/avatars/${username}.jpg`} 
+                    alt={`${username} profile picture`} 
+                />
                 <p className="font-bold">{username}</p>
             </Link>
         </div>
