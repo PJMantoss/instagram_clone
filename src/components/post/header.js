702342366b602e90a142003025a3040e7bc83a4b
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-10 16:55:12 +0100

    Update header.js

--- src/components/post/header.js
+++ src/components/post/header.js
@@ -7,7 +7,7 @@ export default function Header({ username }){
         <div className="flex items-center">
             <Link to={`./p/${username}`} className="flex items-center">
                 <img 
-                    className="" 
+                    className="rounded-full flex h-8 w-8 mr-3" 
                     src={`./images/avatars/${username}.jpg`} 
                     alt={`${username} profile picture`} 
                 />
