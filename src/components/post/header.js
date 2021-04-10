ba185b0231161593e88595425d927dbf3fbd465c
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-10 16:44:09 +0100

    Update header.js

--- src/components/post/header.js
+++ src/components/post/header.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 export default function Header({ username }){
   return(
     <div className="flex border-b h-4 p-4 py-8">
-        <p className="font-bold">{username}</p>
+        <div className="flex items-center">
+            <p className="font-bold">{username}</p>
+        </div>
     </div>
   )
 };
