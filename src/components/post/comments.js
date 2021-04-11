eca4fa29beb80d3b08f3fdd83d7b2badd5544dc1
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-11 09:09:12 +0100

    Update comments

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import AddComments from './add-comments.js';
 
 export default function Comments({ docId, comments: allComments, posted, commentInput }){
+  const [comments, setComments] = useState(allComments);
+  
   return(
     <div></div>
   )
