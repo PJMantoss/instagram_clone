a18fb70726b9f30ab25d86730f3d2215709bb02e
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-11 09:54:32 +0100

    Update add-comment.js

--- src/components/post/add-comment.js
+++ src/components/post/add-comment.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from 'react';
 import FirebaseContext from '../../context/firebase';
+import UserContext from '../../context/user';
 
 export default function AddComments({}){
   return(
