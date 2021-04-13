b810f77df17c1a0008c13cee127fee6fe158417c
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-13 09:23:56 +0100

    Update suggestions.js

--- src/components/sidebar/suggestions.js
+++ src/components/sidebar/suggestions.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect, memo } from 'react';
 import Skeleton from 'react-loading-skeleton';
 
 const Suggestions = ({ userId }) => {
+  const [profiles, setProfiles] = useState('');
+  
   return(<div></div>);
 }
 
