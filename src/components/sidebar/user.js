36ff675bd080100d8198d319ccac75899b258a4a
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-13 08:41:51 +0100

    Update user.js

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const user = () => !username || !fullname ? () : ();
+const user = () => !username || !fullname ? (
+  
+                    ) : (
+  
+                    );
