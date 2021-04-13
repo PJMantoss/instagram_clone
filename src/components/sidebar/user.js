2839cf7cb2a309900bd28916adc11542c9c76c4a
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-13 09:01:46 +0100

    Update user.js

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -6,7 +6,7 @@ const User = ({ username, fullName }) => !username || !fullName ? (
                         <Skeleton count={1} height={61} />
                     ) : (
                         <Link to={`/p/${username}`} className="grid grid-cols-5 gap-4 mb-4 items-center">
-                            <div className="flex items-center">
+                            <div className="flex items-center justify-between col-span-1">
                                 <img />
                             </div>
                         </Link>
