9585c462537a6ce9ff86ccfdd86cc9786ce1cb7c
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-13 09:00:44 +0100

    Update user.js

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -6,7 +6,9 @@ const User = ({ username, fullName }) => !username || !fullName ? (
                         <Skeleton count={1} height={61} />
                     ) : (
                         <Link to={`/p/${username}`} className="grid grid-cols-5 gap-4 mb-4 items-center">
-                            <p>{username}</p>
+                            <div className="">
+                                <img />
+                            </div>
                         </Link>
                     );
 
