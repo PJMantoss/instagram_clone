0796e1f38bb1c6d02437994e96e8a594715c5013
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-13 09:28:09 +0100

    Update suggestions.js

--- src/components/sidebar/suggestions.js
+++ src/components/sidebar/suggestions.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, memo } from 'react';
 import Skeleton from 'react-loading-skeleton';
+import { getSuggestedProfiles } from '../../services/firebase';
 
 const Suggestions = ({ userId }) => {
   const [profiles, setProfiles] = useState(null);
   
   useEffect(() => {
+    async function suggestedProfiles(){}
   }, [])
   
   return(<div></div>);
