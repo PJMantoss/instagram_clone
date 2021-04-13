5733e4d6356cbe0cc5802d6a5dbbeb5897a6da8c
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-13 08:44:41 +0100

    Update user.js

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Skeleton from 'react-loading-skeleton';
 
 const user = () => !username || !fullname ? (
-                       <Skeleton count={} />
+                       <Skeleton count={} height={} />
                     ) : (
   
                     );
