11c2d72551b7e1495e3d747e10e021f68f39bca2
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-13 08:48:55 +0100

    Update user.js

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -5,6 +5,6 @@ import Skeleton from 'react-loading-skeleton';
 const user = () => !username || !fullname ? (
                         <Skeleton count={1} height={61} />
                     ) : (
-                        <Link to={`/p/${username}`} className={}>
+                        <Link to={`/p/${username}`} className="grid-grid-cols-5">
                         </Link>
                     );
