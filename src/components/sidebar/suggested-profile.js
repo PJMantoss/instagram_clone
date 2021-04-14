4efa69217877b5cfedfcba0cb77143980ead7fc2
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-14 10:29:22 +0100

    Update suggested-profile.js

--- src/components/sidebar/suggested-profile.js
+++ src/components/sidebar/suggested-profile.js
@@ -5,7 +5,7 @@ export default function SuggestedProfile({ userDocId, username, profileId, userI
   const [followed, setFollowed] = useState(false);
   
   return !followed ? (
-          <div className="flex">
+          <div className="flex flex-row items-center">
               <img />
               <Link></Link>
           </div>
