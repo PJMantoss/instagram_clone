97cc2b0fe1841afdf3bd9bce7ed09d4787998fc6
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-14 10:25:31 +0100

    Update suggested-profile.js

--- src/components/sidebar/suggested-profile.js
+++ src/components/sidebar/suggested-profile.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
 export default function SuggestedProfile({ userDocId, username, profileId, userId }){
+  const [followed, setFollowed] = useState(false);
+  
   return();
 };
