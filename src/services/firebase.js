466f0591723517f02e2224e256a3cda64fa0ee14
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-15 09:41:51 +0100

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -61,5 +61,5 @@ async function getSuggestedProfiles(userId){
           .filter(profile => profile.userId === userId)
     
     return result.docs
-          .map(user => ({  }))
+          .map(user => ({ ...user.data(), docId: user.id }))
 }
