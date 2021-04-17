03f7ce0585c14265209bddf9de861c7786dc7d6e
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-17 09:54:33 +0100

    Update profile.js

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
+import Header from '../components/header';
 import * as ROUTES from '../constants/routes';
 
 export default function Profile(){
