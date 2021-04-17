402817b5c034d456452e0001056a0530b4e41706
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-17 10:22:49 +0100

    Update profile.js

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -25,6 +25,7 @@ export default function Profile(){
     }, [username, history]);
     
     return userExists ? (
-        <div></div>
+        <div className="bg-gray">
+        </div>
     ) : ();
 }
