620a25fce1c9809ec9dafb65053df0803e6cc863
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-17 09:56:06 +0100

    Update profile.js

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import Header from '../components/header';
 import UserProfile from '../components/profile';
+import { getUserByUsername } from '../services/firebase';
 import * as ROUTES from '../constants/routes';
 
 export default function Profile(){
