26724b1a204033075d8461c6bf5c33c8ef3b7152
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-17 10:22:08 +0100

    Update profile.js

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -24,7 +24,7 @@ export default function Profile(){
         chekUserExistsToLoadProfile();
     }, [username, history]);
     
-    return(
-        <h1>Hello from Profile!</h1>
-    )
+    return userExists ? (
+        <div></div>
+    ) : ();
 }
