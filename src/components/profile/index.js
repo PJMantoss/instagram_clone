1b09f5e74d8427535dd1e56b63d75991ff13f1e0
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-18 10:01:24 +0100

    Update index.js

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -2,6 +2,8 @@ import React, { useReducer, useEffect } from 'react';
 import Header from './header';
 import Photos from './photos';
 
+const reducer = (state, newState) => ({ ...state, ...newState });
+
 export default function Profile({ username }){
   return(
     <>
