176390426c4a4686e492868f9252fa45119b47a6
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-18 10:04:17 +0100

    Update index.js

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -3,6 +3,11 @@ import Header from './header';
 import Photos from './photos';
 
 const reducer = (state, newState) => ({ ...state, ...newState });
+const initialState = {
+  profile: {},
+  photosCollection: [],
+  followerCount
+}
 
 export default function Profile({ username }){
   return(
