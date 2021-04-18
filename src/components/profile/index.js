ea4e763408d5c4ecffcdd105cdd24a45324dd5cd
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-18 10:14:04 +0100

    Update index.js

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -12,7 +12,7 @@ const initialState = {
 export default function Profile({ username }){
   const [{ profile, photosCollection, followerCount }, dispatch] = useReducer(reducer, initialState);
   
-  //think about caching here in localstorage so we don't make a network call we have already made previously (task)
+  //Do caching here in localstorage so we don't make a network call we have already made previously (task)
   useEffect(() => {
     async function getProfileInfoAndPhotos(){
       const [{ ...user }] = await getUserByUsername(username);
