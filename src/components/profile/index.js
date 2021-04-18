70487f1dd4fb4dd1558cc8c29bb33ea2fdc2c596
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-18 10:17:22 +0100

    Update index.js

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -17,6 +17,7 @@ export default function Profile({ username }){
   useEffect(() => {
     async function getProfileInfoAndPhotos(){
       const [{ ...user }] = await getUserByUsername(username);
+      console.log(user);
     };
     
     getProfileInfoAndPhotos();
