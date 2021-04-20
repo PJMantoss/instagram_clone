b7a2f0aa3fec329d779b613b02f7dfb355d8e97a
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-20 08:53:50 +0100

    Update photos.js

--- src/components/profile/photos.js
+++ src/components/profile/photos.js
@@ -4,10 +4,11 @@ import Skeleton from 'react-loading-skeleton';
 export default function Photos({ photos }){
   return(
     <div>
-        <Skeleton count={} width={} height={} />
+        <Skeleton count={9} width={320} height={400} />
         {photos == 0 ? (
           'No Posts Yet'
         ) : (
+           photos.map()
         )}
     </div>
   )
