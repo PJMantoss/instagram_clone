fe408482a89f77a4f6424abc1a9e3b088677fcc9
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-20 09:16:24 +0100

    Update photos.js

--- src/components/profile/photos.js
+++ src/components/profile/photos.js
@@ -4,13 +4,14 @@ import Skeleton from 'react-loading-skeleton';
 export default function Photos({ photos }){
   return(
     <div>
-        <Skeleton count={9} width={320} height={400} />
         {!photos ? (
            <>
-               {[].map() => ()}
+               {[...new, Array(9)].map((_, index) => (
+                   <Skeleton count={9} width={320} height={400} />
+               ))}
            </>
           'No Posts Yet'
-        ) : (
+        ) : photos.length > 0 ? (
            photos.map(photo => photo.docId)
         )}
     </div>
