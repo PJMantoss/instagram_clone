27216fb3dde8dd6d15662c85027cbe8ec28eb0aa
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-20 09:31:41 +0100

    Update photos.js

--- src/components/profile/photos.js
+++ src/components/profile/photos.js
@@ -4,19 +4,21 @@ import Skeleton from 'react-loading-skeleton';
 export default function Photos({ photos }){
   return(
     <div className="h-16 border-t border-gray mt-12 pt-4">
-        {!photos ? (
-           <>
-               {[...new, Array(9)].map((_, index) => (
-                   <Skeleton key={index} count={1} width={320} height={400} />
-               ))}
-           </>
-        ) : photos.length > 0 ? (
-          photos.map(photo => (
-            <div key={photo.docId} className="relative group grid-cols-3">
-                <img src={photo.imageSrc} alt={photo.caption} />
-            </div>
-          ))
-        ) : null}
+        <div className="grid grid-cols-3">
+            {!photos ? (
+               <>
+                   {[...new, Array(9)].map((_, index) => (
+                       <Skeleton key={index} count={1} width={320} height={400} />
+                   ))}
+               </>
+            ) : photos.length > 0 ? (
+              photos.map(photo => (
+                <div key={photo.docId} className="relative group">
+                    <img src={photo.imageSrc} alt={photo.caption} />
+                </div>
+              ))
+            ) : null}
+        </div>
         
         {!photos || (photos.length === 0 && <p className="text-center text-2xl">No Photos Yet</p>)}
     </div>
