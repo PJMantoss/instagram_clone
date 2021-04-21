f1cb58def6045174df479ab29c1948357b1eae9a
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-21 09:48:52 +0100

    Update header.js

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -9,6 +9,8 @@ export default function Header({
 }){
   return(
     <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+       <div className="container"></div>
+       <div className="flex"></div>
     </div>
   )
 };
