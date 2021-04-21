f7e003be444eead48501c3b53b6bb6fb89aa260e
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-21 09:46:58 +0100

    Update header.js

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -8,7 +8,7 @@ export default function Header({
   username
 }){
   return(
-    <div className="grid grid-cols-3">
+    <div className="grid grid-cols-3 gap-4 justify-between mx-auto">
     </div>
   )
 };
