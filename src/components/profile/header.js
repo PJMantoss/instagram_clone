93c5903e5178e96ff1722af1b47cfefe2e26ad55
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-24 16:05:30 +0100

    Update header.js

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import useuser from '../../hooks/use-user';
+import { toggleFollow } from '../../services/firebase';
 
 export default function Header({
   photosCount,
