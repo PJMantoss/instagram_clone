fd0620f883ab5482d24549bec57b5c64b762d972
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-26 10:56:02 +0100

    Update is-user-logged-in.js

--- src/helpers/is-user-logged-in.js
+++ src/helpers/is-user-logged-in.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-export default function isUserLoggedIn({ user, children, ...rest }){
+export default function isUserLoggedIn({ user, loggedInPath, children, ...rest }){
   return(
     <Route />
   )
