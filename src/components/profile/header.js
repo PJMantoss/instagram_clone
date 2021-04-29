8ab77b16b036b4e217a7352cf76602caab118a96
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-29 09:46:32 +0100

    Update header.js

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -13,7 +13,7 @@ export default function Header({
   
   const { user } = useUser();
   const [isFollowingProfile, setIsFollowingProfile] = useState(false);
-  const activeBtnFollow = user && user.username && user.username !== username;
+  const activeBtnFollow = user && user.username && user.username !== profileUsername;
   
   const handleToggleFollow = async () => {
     setIsFollowingProfile(isFollowingProfile => !isFollowingProfile);
@@ -41,14 +41,14 @@ export default function Header({
        <div className="container flex justify-center">
            <img
                className="rounded-full h-40 w-40 flex"
-               alt={`${username} profile picture`}
-               src={`/images/avatars/${username}.jpg`}
+               alt={`${profileUsername} profile picture`}
+               src={`/images/avatars/${profileUsername}.jpg`}
            />
        </div>
     
        <div className="flex items-center justify-center flex-col col-span-2">
            <div className="container flex items-center">
-               <p className="text-2xl mr-4">{username}</p>
+               <p className="text-2xl mr-4">{profileUsername}</p>
                {activeBtnFollow && (
                    <button
                        className="bg-blue-500 font-bold text-sm rounded text-white w-20 h-8"
