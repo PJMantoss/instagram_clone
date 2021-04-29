e349ae34697fdf3a8d338c0f09c782edec68750c
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2021-04-29 08:31:39 +0100

    Update protected-route.js

--- src/helpers/protected-route.js
+++ src/helpers/protected-route.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import * as ROUTES from '../constants/routes'
 
+export default function ProtectedRoute({ user, children, ...rest }){
+  return(
+    <Route
+        {...rest}
+        render={() => {
+             if(!user){
+               return children;
+             }
+    
+            if(user){
+              return(
+                <Redirect to={{ pathname: loggedInPath }} />
+              )
+            }
+    
+           return null;
+         }}
+    />
+  );
+}
